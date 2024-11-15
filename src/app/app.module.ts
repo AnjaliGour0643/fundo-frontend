dce65ca5902e4bde1e1e0735d1b0e19b38d73a75
Anjali Gour <gour26000@gmail.com>
2024-11-15 10:00:33 +0530

    [Anjali] REFACTORED : implemented email and password validation in user login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SignupComponent } from './components/signup/signup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,9 @@ import { SignupComponent } from './components/signup/signup.component';
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  registerForm!: FormGroup;
+  submitted = false;
+  showPass = "text"
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(6)]],
+      });
+  }
+
+  // convenience getter for easy access to form fields
+  get regFormControls() { return this.registerForm.controls; }
+
+  handleLogin(){
+    console.log("Email required error:", this.regFormControls["email"]["errors"]?.["required"])
+    console.log("Email format error:", this.regFormControls["email"]["errors"]?.["email"])
+
+    console.log("Password required error:", this.regFormControls["password"]["errors"]?.["required"]);
+    console.log("Password minLength error:", this.regFormControls["password"]["errors"]?.["minlength"])
+  }
+  
 }
