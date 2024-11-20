a35bbb07adfbd675d04485c8521373aafbbff584
Anjali Gour <gour26000@gmail.com>
2024-11-21 00:35:46 +0530

    [Anjali] FEAT : added api to store note in database in add-note.component.ts

--- src/app/components/add-note/add-note.component.ts
+++ src/app/components/add-note/add-note.component.ts
@@ -1,6 +1,8 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { HttpService } from 'src/app/services/http-service/http.service';
 import { REMINDER_ICON, COLLABRATOR_ICON, COLOR_PALATTE_ICON, IMG_ICON, ARCHIVE_ICON, MORE_ICON, DELETE_FOREVER_ICON, RESTORE_ICON, UNARCHIVE_ICON } from 'src/assets/svg-icons';
 
 @Component({
@@ -17,7 +19,7 @@ export class AddNoteComponent {
 
   @Output() updateList = new EventEmitter()
 
-  constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
+  constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer, private httpService: HttpService) {
     iconRegistry.addSvgIconLiteral('reminder-icon', sanitizer.bypassSecurityTrustHtml(REMINDER_ICON));
     iconRegistry.addSvgIconLiteral('collabrator-icon', sanitizer.bypassSecurityTrustHtml(COLLABRATOR_ICON));
     iconRegistry.addSvgIconLiteral('color-palatte-icon', sanitizer.bypassSecurityTrustHtml(COLOR_PALATTE_ICON));
@@ -30,11 +32,23 @@ export class AddNoteComponent {
   }
 
   addNoteToggle(action: string){
-    this.addnote = !this.addnote
-    if(action==='save'){
+    let title = this.title;
+    let description = this.description;
+    let color = 'white';
 
-      //add the apicall
-      
+    this.addnote = !this.addnote
+
+    const header = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`);
+
+    if(action==='save'){
+      this.httpService.postApiCall('/api/v1/notes', {title, description, color}, header).subscribe({
+        next: (res) => {
+          console.log(res)
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      })
       console.log(this.title, this.description)
       this.updateList.emit({data: this.title, action: 'add'})
     }
