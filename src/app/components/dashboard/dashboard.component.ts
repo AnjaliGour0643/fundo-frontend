efe9d74d8a98ee61de58272cfe1948cb4250d3f3
Anjali Gour <gour26000@gmail.com>
2024-11-25 18:40:21 +0530

    [Anjali] REFACTORED : added UI for dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -8,14 +8,47 @@ import { MENU_ICON } from 'src/assets/svg-icons';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-
 export class DashboardComponent {
-  drawerState: boolean = false
-  
+  drawerState: boolean = false;
+  selectedSection: string = 'notes';
+  router: any;
+
   constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
     iconRegistry.addSvgIconLiteral('menu-icon', sanitizer.bypassSecurityTrustHtml(MENU_ICON));
   }
-  handleDrawerClick(){
-    this.drawerState = !this.drawerState
+
+  handleDrawerClick() {
+    this.drawerState = !this.drawerState;
+  }
+
+  navigateToSection(section: string) {
+    this.selectedSection = section;
+    this.router.navigate([`/dashboard/${section}`]); // Navigate to the respective child route
   }
 }
+
+
+
+
+
+// import { Component } from '@angular/core';
+// import { MatIconRegistry } from '@angular/material/icon';
+// import { DomSanitizer } from '@angular/platform-browser';
+// import { MENU_ICON } from 'src/assets/svg-icons';
+
+// @Component({
+//   selector: 'app-dashboard',
+//   templateUrl: './dashboard.component.html',
+//   styleUrls: ['./dashboard.component.scss']
+// })
+
+// export class DashboardComponent {
+//   drawerState: boolean = false
+  
+//   constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
+//     iconRegistry.addSvgIconLiteral('menu-icon', sanitizer.bypassSecurityTrustHtml(MENU_ICON));
+//   }
+//   handleDrawerClick(){
+//     this.drawerState = !this.drawerState
+//   }
+// }
