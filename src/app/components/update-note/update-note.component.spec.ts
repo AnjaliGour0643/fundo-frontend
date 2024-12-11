79059c8b3046bdc64c5141d3af7095be7d628a6a
Anjali Gour <gour26000@gmail.com>
2024-12-11 17:42:47 +0530

    [Anjali] ADDED : added test cases for update-note component

--- src/app/components/update-note/update-note.component.spec.ts
+++ src/app/components/update-note/update-note.component.spec.ts
@@ -1,15 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UpdateNoteComponent } from './update-note.component';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('UpdateNoteComponent', () => {
   let component: UpdateNoteComponent;
   let fixture: ComponentFixture<UpdateNoteComponent>;
+  let dialogRefMock: jasmine.SpyObj<MatDialogRef<UpdateNoteComponent>>;
+  let httpServiceMock: jasmine.SpyObj<any>;
 
   beforeEach(() => {
+    dialogRefMock = jasmine.createSpyObj('MatDialogRef', ['close', 'backdropClick']);
+    dialogRefMock.backdropClick.and.returnValue(of());
+
+    httpServiceMock = jasmine.createSpyObj('HttpService', ['putApiCall']);
+    httpServiceMock.putApiCall.and.returnValue(of({ success: true }));  
+
     TestBed.configureTestingModule({
-      declarations: [UpdateNoteComponent]
-    });
+      declarations: [UpdateNoteComponent],
+      imports: [
+        BrowserModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientModule,
+        MatIconModule,
+        MatDialogModule,
+        MatMenuModule,
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogRefMock },
+        { provide: MAT_DIALOG_DATA, useValue: { title: 'Test Note', description: 'Test Description', color: '#fff', _id: '1' } },
+        { provide: 'HttpService', useValue: httpServiceMock },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(UpdateNoteComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +53,24 @@ describe('UpdateNoteComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize with provided dialog data', () => {
+    expect(component.title).toBe('Test Note');
+    expect(component.description).toBe('Test Description');
+    expect(component.color).toBe('#fff');
+    expect(component._id).toBe('1');
+  });
+
+  it('should change note color when setColor is called', () => {
+    component.setColor('#000');
+
+    expect(component.color).toBe('#000');
+  });
+
+  it('should handle backdrop click and close dialog', () => {
+    dialogRefMock.backdropClick().subscribe(() => {
+      expect(dialogRefMock.close).toHaveBeenCalled();
+    });
   });
 });
